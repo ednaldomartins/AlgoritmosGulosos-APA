d0bab23aae2af48cae6cff866df3a8c060c2fe42
Ednaldo <martinsednaldo@outlook.com>
2018-09-23 11:48:41 -0300

    Correção do numero do segundo vértice

--- src/infra/Arquivo.java
+++ src/infra/Arquivo.java
@@ -13,52 +13,47 @@ import java.util.ArrayList;
 import java.util.List;
 import model.Aresta;
 
-/**
- * @author Ednaldo
- *  data: 22.09.2018
- */
-public class Arquivo {
+/*******************************************************************************
+ * @author Ednaldo                                                             *
+ *  data: 22.09.2018                                                           *
+ ******************************************************************************/
+public class Arquivo 
+{
+    private final Path loadPath;
+    private final Charset utf8 = StandardCharsets.UTF_8;
     
-    private Path loadPath;
-    private Charset utf8 = StandardCharsets.UTF_8;
-    
-    public Arquivo (String loadPath) {
+    public Arquivo (String loadPath) 
+    {
         this.loadPath = Paths.get(loadPath);
     }
     
-    /**
-     * MST: Minimum Soanning Tree
-     * @return arvore
-     * @throws IOException 
-     */
-    public Aresta [] carregarMST() throws IOException {
-        Aresta [] listaAresta = null;
-        List<Long> listaPesoAresta = new ArrayList<>();
-        
-        try (BufferedReader reader = Files.newBufferedReader(loadPath, utf8)){
+    /***************************************************************************
+     * MST: Minimum Soanning Tree                                              *
+     * i: primeiro vertice                                                     *
+     * j: segundo vertice                                                      *
+     * k: valor do peso da aresta que estah ligando os vertice 'i' e 'j'       *
+     * @return arvore                                                          *
+     * @throws IOException                                                     *
+     **************************************************************************/
+    public List <Aresta> carregarMST() throws IOException 
+    {
+        List<Aresta> listaPesoAresta = new ArrayList<>();
+        try (BufferedReader reader = Files.newBufferedReader(loadPath, utf8))
+        {
             String line = null;
-            while((line = reader.readLine()) != null) {
+            for(int i = 0; (line = reader.readLine()) != null; i++) 
+            {
                 String[] parametro = line.split(" ");
-                //add valores no array dinamico
-                for(int i = 0; i < parametro.length; i++)
-                    listaPesoAresta.add( Long.parseLong(parametro[i]) );
+                for(int j = i+1, k = 0; k < parametro.length; k++, j++)
+                    listaPesoAresta.add(   new Aresta(i, j, Long.parseLong(parametro[k]), 0 )   );
             }
             reader.close();         
         }
         catch(FileNotFoundException ex) {System.err.println("Arquivo não encontrado");}
         catch(NumberFormatException ex) {System.err.println("Erro de formato de numeros");}
-        catch(IOException ex) 
-        {
-            System.err.println("Erro de leitura");
-            ex.printStackTrace();
-        }
-        //FAZER: repassar pesos da aresta para o novo vetor (usar funcao)
-        int tamanhoMST = listaPesoAresta.size();
-        listaAresta = new Aresta [tamanhoMST];
-        for(int i = 0; i < tamanhoMST; i++)
-           listaAresta[i] = new Aresta(10, 10, listaPesoAresta.get(i), 10);
-        //no retorno ainda estah passando o numero de vertices na posicao 0
-        return listaAresta;
+        catch(IOException ex) {System.err.println("Erro de leitura");}
+        
+        return listaPesoAresta;
     }
     
 }
