3f0475e460324e0b050c031682ffadf8948e10ee
Ednaldo <martinsednaldo@outlook.com>
2018-09-24 10:21:08 -0300

    criação do algoritmo de Kruskal

---
+++ src/model/Kruskal.java
@@ -0,0 +1,24 @@
+
+package model;
+
+import java.util.List;
+
+import util.AlgoritmoGuloso;
+
+/*******************************************************************************
+ * @author Ednaldo                                                             *
+ *  date: 23.09.2018                                                           *
+ ******************************************************************************/
+public class Kruskal implements AlgoritmoGuloso{
+
+    @Override
+    public long[] gerarMST(List<Aresta> listaAresta) 
+    {
+        long mst [] = new long [ (int) listaAresta.get(0).getPeso() ];
+        
+        //codigo
+        
+        return mst;
+    }
+    
+}
