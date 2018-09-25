fdaaa71566ebd376b69d252e24281820185d13fa
Ednaldo <martinsednaldo@outlook.com>
2018-09-25 10:47:19 -0300

    Main: teste sem menu

--- src/algoritmosgulosos/AlgoritmosGulosos.java
+++ src/algoritmosgulosos/AlgoritmosGulosos.java
@@ -1,30 +1,30 @@
 
 package algoritmosgulosos;
 
-import infra.Arquivo;
-import java.io.IOException;
-import java.util.List;
-import model.Aresta;
+import control.ControllerMST;
+import model.Kruskal;
 
-/**
- * @author Ednaldo
- *  date: 22.09.2018
- */
+/*******************************************************************************
+ * @author Ednaldo                                                             *
+ *  date: 22.09.2018                                                           *
+ ******************************************************************************/
 public class AlgoritmosGulosos {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws IOException 
+    
+    public static void main(String[] args) 
     {
-        Arquivo arquivo = new Arquivo("..\\AlgoritmosGulosos\\dij10.txt");
-        List <Aresta> list = arquivo.carregarMST();
-        for(int i = 1; i<list.size(); i++)
-        {
-            System.out.println( "(" + list.get(i).getV1() + ")" + 
-                                "______" + list.get(i).getPeso() + "______" + 
-                                "(" + list.get(i).getV2() + ")" );
-        }
+//        Arquivo arquivo = new Arquivo("..\\AlgoritmosGulosos\\dij10.txt");
+//        List <Aresta> list = arquivo.carregarMST();
+//        for(int i = 1; i<list.size(); i++)
+//        {
+//            System.out.println( "(" + list.get(i).getV1() + ")" + 
+//                                "______" + list.get(i).getPeso() + "______" + 
+//                                "(" + list.get(i).getV2() + ")" );
+//        }
+        
+        ControllerMST control = new ControllerMST();
+        control.controleMST( new Kruskal() );
+        
+        
     }
     
 }
