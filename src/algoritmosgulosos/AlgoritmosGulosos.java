89e6c7f09a84c67a55ea80dcd9a84367bf1227d3
Ednaldo <martinsednaldo@outlook.com>
2018-09-25 11:59:09 -0300

    Print de resultados

--- src/algoritmosgulosos/AlgoritmosGulosos.java
+++ src/algoritmosgulosos/AlgoritmosGulosos.java
@@ -11,20 +11,12 @@ import model.Kruskal;
 public class AlgoritmosGulosos {
     
     public static void main(String[] args) 
-    {
-//        Arquivo arquivo = new Arquivo("..\\AlgoritmosGulosos\\dij10.txt");
-//        List <Aresta> list = arquivo.carregarMST();
-//        for(int i = 1; i<list.size(); i++)
-//        {
-//            System.out.println( "(" + list.get(i).getV1() + ")" + 
-//                                "______" + list.get(i).getPeso() + "______" + 
-//                                "(" + list.get(i).getV2() + ")" );
-//        }
-        
+    {  
         ControllerMST control = new ControllerMST();
+        control.printLista();
         control.controleMST( new Kruskal() );
-        
-        
+        control.printMST();
+        control.printResultadoMST();
     }
     
 }
