080c30dd4b985a03f024c388ad61ec9c92fe5541
Ednaldo <martinsednaldo@outlook.com>
2018-10-03 11:03:43 -0300

    criação do Objeto Prim

---
+++ src/model/Prim.java
@@ -0,0 +1,18 @@
+
+package model;
+
+import util.AlgoritmoGuloso;
+
+/*******************************************************************************
+ * @author Ednaldo                                                             *
+ *  date: 03.10.2018                                                           *
+ ******************************************************************************/
+public class Prim implements AlgoritmoGuloso {
+
+    @Override
+    public void gerarMST(Aresta[] vetorAresta, Aresta[] arvoreGeradora, int tamanhoVetor) 
+    {
+        
+    }
+    
+}
