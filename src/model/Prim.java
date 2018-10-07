7ad36a4f02d86bfcd130a1545c48cf7ea86ba345
Ednaldo <martinsednaldo@outlook.com>
2018-10-07 00:40:18 -0300

    Algoritmo de Prim funcionando

--- src/model/Prim.java
+++ src/model/Prim.java
@@ -21,16 +21,14 @@ public class Prim implements AlgoritmoGuloso {
                 numeroFloresta = 1 : faz parte do vetor
                 numeroFLoresta = 0 : não faz parte do vetor
             */
-            int numeroFloresta = 1;
             FuncoesVetor.iniciarVetor(mst);
             mst[0] = matrizAresta[0][0];
             mst[0].setV1(-1);
             mst[0].setV2(0);
-            mst[0].setFloresta(numeroFloresta);
-            //Gerar MST. Apontador diz ateh qual posicao a mst[]  foi preenchida
+            mst[0].setFloresta(1);
+            //enquanto nao for uma MST adicionar novas arestas
             while(!isMST(mst))
                 minimo(matrizAresta, mst);
-            
         }
     }
     
@@ -38,34 +36,41 @@ public class Prim implements AlgoritmoGuloso {
     {
         long  menor = Long.MAX_VALUE;
         int verticeI = -1, verticeJ = -1;
-        //while(i < vetor.length  && vetor[ (int)i ].getPeso() < Long.MAX_VALUE)
         for(int v = 0, i = 0; v < vetor.length  && vetor[ (int)i ].getPeso() < Long.MAX_VALUE; v++)
         {
+            //pega o proximo vertice para verificar suas conexoes na matriz
             i = (int) vetor[v].getV2();
             for(int j = 0; j < matriz.length; j++)
             {
+                //sempre pega o menor caminho disponivel entre os vertices conhecidos
                 if(   (matriz[ (int)i ][j].getPeso() != 0)   &&
                       (matriz[ (int)i ][j].getPeso() < menor)   &&
-                      (matriz[ (int)i ][j].getFloresta() == 0)   )
-                {   //se fizer parte da floresta tem q fazer uma troca
+                       isAcyclic(vetor, i, j)   )
+                {   
                     menor = matriz[ (int)i ][j].getPeso();
                     verticeI = (int) i;
                     verticeJ = j;
                 }
-            }
-        }
+            }//fim do for 2
+        }//fim do for 1
         addAresta(matriz, vetor, verticeI, verticeJ);
     }
     
+    /***************************************************************************
+     * Metodo para adicionar a nova aresta na MST. O metodo deve procurar a    *
+     * proxima posicao disponivel no vetor MST.                                *
+     * @param matriz                                                           *
+     * @param vetor                                                            *
+     * @param i                                                                *
+     * @param j                                                                *
+     **************************************************************************/
     private void addAresta(Aresta[][] matriz, Aresta[] vetor, int i, int j)
     {
         int k = 0;
         while(k < vetor.length  && vetor[k].getPeso() < Long.MAX_VALUE)
             k++;
-        
+        //setFloresta(1) ainda estah servindo para o metodo isMST
         vetor[k] = new Aresta(i, j, matriz[i][j].getPeso(), 1);
-        matriz[i][j].setFloresta(1);
-        matriz[j][i].setFloresta(1);
     }
     
     private boolean isMST (Aresta[] vetorMST)
@@ -74,5 +79,29 @@ public class Prim implements AlgoritmoGuloso {
             if(vetorMST[i].getFloresta() == 0)
                 return false;
         return true;
+    }
+    
+    /***************************************************************************
+     * O metodo deve retornar uma resposta verdadeira ou false. O metodo       *
+     * consiste em dizer, se a MST ira continuar acilica mesmo apos a insercao *
+     * de uma Aresta entre os vertices I e J.                                  *
+     * @param vetor                                                            *
+     * @param verticeI                                                         *
+     * @param verticeJ                                                         *
+     * @return                                                                 *
+     **************************************************************************/
+    private boolean isAcyclic (Aresta [] vetor, int verticeI, int verticeJ)
+    {
+        //variaveis para verificar se o vertice I e J ja pertencem ao vetor MST
+        boolean vI = false, vJ = false;
+        for(int k = 0; k < vetor.length  && vetor[k].getPeso() < Long.MAX_VALUE; k++)
+        {
+            if( verticeI == vetor[k].getV1() || verticeI == vetor[k].getV2() )
+                vI = true;
+            if( verticeJ == vetor[k].getV1() || verticeJ == vetor[k].getV2() )
+                vJ = true;
+        }
+        // (true,true)=false : se os 2 vertices ja pertences a mst, entao false
+        return !(vI && vJ);
     }
 }
