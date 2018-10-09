50087f9153cc3f233ca7bbcdfb3945dc5fe3598b
Ednaldo <martinsednaldo@outlook.com>
2018-10-09 12:26:15 -0300

    Correção do nome do método.

--- src/model/Prim.java
+++ src/model/Prim.java
@@ -8,9 +8,8 @@ import util.FuncoesVetor;
  * @author Ednaldo                                                             *
  *  date: 03.10.2018                                                           *
  ******************************************************************************/
-public class Prim implements AlgoritmoGuloso  
+public class Prim extends Grafo implements AlgoritmoGuloso  
 {
-    private Grafo grafo;
     @Override
     public void gerarSolucaoSubOtima(Aresta[][] matrizAresta, Aresta[] mst, int tamanhoMatriz) 
     {
@@ -28,8 +27,8 @@ public class Prim implements AlgoritmoGuloso
             mst[0].setV2(0);
             mst[0].setFloresta(1);
             //enquanto nao for uma MST adicionar novas arestas
-            while(!grafo.isFull(mst))
-                grafo.arestaMinima(matrizAresta, mst);
+            while(!super.isFull(mst))
+                super.arestaMinima(matrizAresta, mst);
         }
     }
     
